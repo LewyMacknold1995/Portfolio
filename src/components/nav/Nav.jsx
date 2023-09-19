85336a7e23cae8957b57a5bee4701740d573013b
Lewy Macknold <lewy@Lewys-MacBook.local>
2023-09-19 13:12:55 +0100

    finished nav bar componenet

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
 import './nav.css'
+import {AiOutlineHome} from 'react-icons/ai'
+import {AiOutlineUser} from 'react-icons/ai'
+import {BsBook} from 'react-icons/bs'
+import {RiServiceLine} from 'react-icons/ri'
+import {IoIosContact} from 'react-icons/io'
+import {useState} from 'react'
 
 const Nav = () => {
+  const [activeNav, setActiveNav] = useState('#'); 
   return (
-    <div>Nav</div>
+    <nav>
+      <a href="#" onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}><AiOutlineHome/></a>
+      <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><AiOutlineUser/></a>
+      <a href="#experience" onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}><BsBook/></a>
+      <a href="#portfolio" onClick={() => setActiveNav('#portfolio')} className={activeNav === '#portfolio' ? 'active' : ''}><RiServiceLine/></a>
+      <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><IoIosContact/></a>
+    </nav>
   )
 }
 
--- src/components/testimonials/Testimonial.jsx
+++ src/components/testimonials/Testimonial.jsx
@@ -3,7 +3,7 @@ import './testimonial.css'
 
 const Testimonial = () => {
   return (
-    <div>Testimonial</div>
+    <section id='testimonials'>Testimonial</section>
   )
 }
 
